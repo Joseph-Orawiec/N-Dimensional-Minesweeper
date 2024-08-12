ba68fc2315093f2175d61a7571191478d7e81537
Joseph Orawiec <143203535+Joseph-Orawiec@users.noreply.github.com>
2024-08-11 22:55:03 -0500

    moved adj vect logic to own function
    and doesn't recalculate

--- scenes/mine_field/mine_field.gd
+++ scenes/mine_field/mine_field.gd
@@ -8,8 +8,7 @@ var node_dict: Dictionary = {} # node arr (parallel dictionary)
 var cell: PackedScene = preload("res://scenes/cell/cell.tscn")
 var dimension: Array[int] # size of the mine field
 
-var adjacency_vectors = []
-#var adjacency_vector_dictionary: Dictionary = {} #holds arrays of adjacency vectors which are useful to loop through for a lot of things
+var adjacency_vector_dictionary: Dictionary = {} #holds arrays of adjacency vectors which are useful to loop through for a lot of things
 
 
 # Called when the node enters the scene tree for the first time.
@@ -29,9 +28,42 @@ func _process(delta):
 func new_game(dimension: Array[int], mines: int):
 	var d: int = len(dimension) # Nth dimension
 	
+	generate_adjacency_vectors(d)
+	print(adjacency_vector_dictionary)
+	
+	var grid_container = GridContainer.new()
+	
+	grid_container.add_theme_constant_override('h_separation', 0)
+	grid_container.add_theme_constant_override('v_separation', 0)
+	grid_container.columns = dimension[0]
+	grid_container.size = Vector2(dimension[0], dimension[1]) * 50
+	
+	for y in dimension[1]:
+		for x in dimension[0]:
+			var temp_id: Array[int] = dimension.duplicate()
+			temp_id[0] = x
+			temp_id[1] = y
+			
+			field_dict[temp_id] = 0
+			node_dict[temp_id] = cell.duplicate()
+			
+			var current_cell = node_dict[temp_id]
+			
+			grid_container.add_child(current_cell)
+			
+	add_child(grid_container)
+
+# handles the logic of generating adjacency vectors for any dimension
+func generate_adjacency_vectors(dimension: int):
+	# don't compute if previously generated
+	if adjacency_vector_dictionary.get(dimension, null) != null:
+		return
+	
+	var adjacency_vectors: Array[Array] = []
+	
 	# Setup binary counter
 	var arr: Array[int] = []
-	arr.resize(d)
+	arr.resize(dimension)
 	arr.fill(-1)
 	
 	
@@ -48,40 +80,17 @@ func new_game(dimension: Array[int], mines: int):
 				isDone = true
 				break
 			
+			# set value back to "0" and increase the next
 			arr[index] = -1
 			arr[index + 1] += 1
 			
-			# about to rollover last index
-			
-				
+			# move onto checking the next bit to the right
 			index += 1 #finally, increase
+			
 	# Remove the [0, 0, ... 0] case as that's just itself and happens to always be in the middle of the rray
 	adjacency_vectors.remove_at(len(adjacency_vectors)/2) 
 	
-	
-	
-	
-	var grid_container = GridContainer.new()
-	
-	grid_container.add_theme_constant_override('h_separation', 0)
-	grid_container.add_theme_constant_override('v_separation', 0)
-	grid_container.columns = dimension[0]
-	grid_container.size = Vector2(dimension[0], dimension[1]) * 50
-	
-	for y in dimension[1]:
-		for x in dimension[0]:
-			var temp_id: Array[int] = dimension.duplicate()
-			temp_id[0] = x
-			temp_id[1] = y
-			
-			field_dict[temp_id] = 0
-			node_dict[temp_id] = cell.instantiate()
-			
-			var current_cell = node_dict[temp_id]
-			
-			grid_container.add_child(current_cell)
-			
-	add_child(grid_container)
+	adjacency_vector_dictionary[dimension] = adjacency_vectors
 
 # I could have made an N-dimensional vector class but this is the only method i really need
 func add(v: Array[int], u):
